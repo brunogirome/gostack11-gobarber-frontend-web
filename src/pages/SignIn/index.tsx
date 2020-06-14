4396b641ecf731fa4fa5c9a73940a3f005335c18
Bruno Girome <brunogirome@gmail.com>
2020-06-14 19:57:57 -0300

    Login page validation

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -7,31 +11,60 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+interface registerData {
+  email: string;
+  password: string;
+}
 
-      <form>
-        <h1>SignIn your season</h1>
-        <Input name="email" icon={FiMail} placeholder="E-Mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
-        <Button type="submit">Login</Button>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-        <a href="forgot">Forget my passowrd</a>
-      </form>
-      <a href="register">
-        <FiLogIn />
-        Create account
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+  const handleSubmit = useCallback(async (data: registerData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email is required')
+          .email('Invalid e-mail'),
+        password: Yup.string().required('Password is required'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>SignIn your season</h1>
+          <Input name="email" icon={FiMail} placeholder="E-Mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+          <Button type="submit">Login</Button>
+
+          <a href="forgot">Forget my passowrd</a>
+        </Form>
+        <a href="register">
+          <FiLogIn />
+          Create account
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
