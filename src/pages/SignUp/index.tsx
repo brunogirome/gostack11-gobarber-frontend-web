e12202133a7d43e3ab2c8955d978c373fadc3c03
Bruno Girome <brunogirome@gmail.com>
2020-06-14 18:39:39 -0300

    Early YUP integration

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -8,10 +9,30 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface registerData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: registerData) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        email: Yup.string()
+          .required('Email is required')
+          .email('Use a valid mail'),
+        password: Yup.string().min(6, 'Minimun 6 characters'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <Container>
