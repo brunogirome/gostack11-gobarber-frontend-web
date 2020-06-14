b66bac00c742f4e7bef7d55633e06dddd6d29d06
Bruno Girome <brunogirome@gmail.com>
2020-06-13 23:32:52 -0300

    Encapsulated components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
-
-import { Container, Content, Background } from './styles';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -12,9 +14,14 @@ const SignIn: React.FC = () => (
 
       <form>
         <h1>SignIn your season</h1>
-        <input placeholder="E-Mail" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        <Input name="email" icon={FiMail} placeholder="E-Mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Password"
+        />
+        <Button type="submit">Login</Button>
 
         <a href="forgot">Forget my passowrd</a>
       </form>
