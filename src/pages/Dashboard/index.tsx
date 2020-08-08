ddef0f2dc0b0c7a7e78c304b2ac04db5a9317ba0
Bruno Girome <brunogirome@gmail.com>
2020-08-08 16:13:46 -0300

    Creation of styles and structure of dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -28,6 +41,101 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Schedule</h1>
+          <p>
+            <span>Today</span>
+            <span>Day 06</span>
+            <span>Monday</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Next Appointment</strong>
+            <div>
+              <img
+                src="https://avatarfiles.alphacoders.com/156/thumb-156175.png"
+                alt="Oreimo avatar"
+              />
+
+              <strong>Ana Flávia</strong>
+              <span>
+                <FiClock />
+                06:06
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Morning</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:33
+              </span>
+
+              <div>
+                <img
+                  src="https://avatarfiles.alphacoders.com/156/thumb-156175.png"
+                  alt="Oreimo avatar"
+                />
+
+                <strong>Ana Flávia</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:33
+              </span>
+
+              <div>
+                <img
+                  src="https://avatarfiles.alphacoders.com/156/thumb-156175.png"
+                  alt="Oreimo avatar"
+                />
+
+                <strong>Ana Flávia</strong>
+              </div>
+            </Appointment>
+
+            <strong>Afternoon</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:33
+              </span>
+
+              <div>
+                <img
+                  src="https://avatarfiles.alphacoders.com/156/thumb-156175.png"
+                  alt="Oreimo avatar"
+                />
+
+                <strong>Ana Flávia</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:33
+              </span>
+
+              <div>
+                <img
+                  src="https://avatarfiles.alphacoders.com/156/thumb-156175.png"
+                  alt="Oreimo avatar"
+                />
+
+                <strong>Ana Flávia</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar>Calendar</Calendar>
+      </Content>
     </Container>
   );
 };
